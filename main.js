abeb6884f5f898b513f4e04214a735cac72a39c6
Douglas <u19049782@tuks.co.za>
2022-05-29 18:14:00 +0200

    Voting system flow setup

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ Moralis.start({ serverUrl, appId });
 
 /* Authentication code */
 async function login() {
+    console.log("logging in");
     let user = Moralis.User.current();
     if (!user) {
         user = await Moralis.authenticate({
             signingMessage: "Log in using Moralis",
         })
             .then(function (user) {
+                window.location.href = "election.html";
                 console.log("logged in user:", user);
                 console.log(user.get("ethAddress"));
             })
@@ -21,9 +23,11 @@ async function login() {
 }
 
 async function logOut() {
+    console.log("logging out");
     await Moralis.User.logOut();
     console.log("logged out");
+    window.location.href = "index.html";
 }
 
-document.getElementById("btn-login").onclick = login;
-document.getElementById("btn-logout").onclick = logOut;
+// document.getElementById("btn-login").onclick = login;
+// document.getElementById("btn-logout").onclick = logOut;
